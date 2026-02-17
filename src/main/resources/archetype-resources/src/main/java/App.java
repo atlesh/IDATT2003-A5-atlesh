8e63e916dca43081121c18b4722b045e76d4f041
atlesh <atlesh@stud.ntnu.no>
2026-02-17 13:50:38 +0100

    Initial Upload

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $atlesh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
